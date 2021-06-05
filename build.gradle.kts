ddd2960c8422f5f3ed32a52bdeec7f7ae60e1152
David <davidschlachter96@gmail.com>
2021-06-05 14:38:38 -0700

    Setup publishing to rsps-studios-private repository.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.10"
     `java-gradle-plugin`
+    `maven-publish`
 }
 
 group = "com.javatar"
@@ -12,4 +13,21 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+        }
+        repositories {
+            maven {
+                url = uri("http://legionkt.com:8085/repository/rsps-studios-private/")
+                credentials {
+                    username = project.properties["myNexusUsername"] as String
+                    password = project.properties["myNexusPassword"] as String
+                }
+            }
+        }
+    }
 }
