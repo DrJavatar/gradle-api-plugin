875086347557b6b40f208e11434fe3335d45e8f8
David <davidschlachter96@gmail.com>
2021-06-05 14:42:59 -0700

    Upgrade to gradle 7.0.2 and start on gradle plugin development.

---
+++ src/main/kotlin/com/javatar/gradle/plugin/RspsStudiosPlugin.kt
@@ -0,0 +1,10 @@
+package com.javatar.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class RspsStudiosPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
